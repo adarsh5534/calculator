2c96b8478f3f3028aa2136e05617dfa6f7f64dfc
adarsh5534 <aipe3647@gmail.com>
2022-11-19 03:38:53 +0530

    cal

---
+++ cal.js
@@ -0,0 +1,10 @@
+function display(num){
+ result.value+=num
+}
+function allclear(){
+    result.value=''
+}function evalexp(){
+    result.value=eval(result.value)
+}function clearqq(){
+    result.value=result.value.slice(0,-1)
+}
